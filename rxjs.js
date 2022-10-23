9b1384a9f7a28ee607084c3d7373807b0d4b6a77
Prem N <43260360+prem-npk@users.noreply.github.com>
2022-10-23 11:25:44 +0530

    Update rxjs.js
    pipe logics added

--- rxjs.js
+++ rxjs.js
@@ -1,11 +1,15 @@
 import { Observable } from "rxjs";
 function asyncStream(Observer) {
   var tr = setInterval(() => {
-    Observer.next(Math.random());
+    Observer.next(Math.random()*10);
   }, 1000);
 }
-var observ = Observable.create(asyncStream);
-var subcription = observ.subcribe((res) => Listener(res));
+var mainStreamObservable = Observable.create(asyncStream);
+var step1Stream=mainStreamObservable.pipe(map(x=>math.round()));
+var step2Stream=step1Stream.pipe(filter(x=>(x>4));
+                                 
+var subcription = step2Stream.subcribe((res) => Listener(res));
+
 function Listener(res) {
   console.log(res);
   subcription.unsubcribe();
